14a3389569fa96a7f113b1eab1ff3bd7977b7254
CustomFont <jjddff12@gmail.com>
2023-05-01 13:36:15 -0400

    update comment

--- backend/database.go
+++ backend/database.go
@@ -13,8 +13,8 @@ type Env struct {
 }
 
 // ConnectDB tries to connect DB and on succcesful it returns
-// DB connection string and nil error, otherwise return empty DB and the corresponding error.
 func ConnectDB() (*sql.DB, error) {
+	// DB connection string and nil error, otherwise return empty DB and the corresponding error.
 	db, err := sql.Open("postgres", "user=postgres password=password dbname=tasks sslmode=disable")
 	if err != nil {
 		log.Printf("failed to connect to database: %v", err)
