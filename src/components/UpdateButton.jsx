72c86963e6d4f7fa6f47c1fbee4299af5687c8e0
Alex Moran <alexmoran@Alexs-MacBook-Pro.local>
2023-05-03 00:51:45 -0400

    added useeffect for defaultValues

--- src/components/UpdateButton.jsx
+++ src/components/UpdateButton.jsx
@@ -4,39 +4,51 @@ import Modal from 'react-modal';
 import axios from 'axios';
 
 export default function UpdateButton(props){
-    const itemListContext = useContext(ItemListContext)
-    const setToDoItems = itemListContext.setToDoItems;
-    const toDoItems = itemListContext.toDoItems;
-    Modal.setAppElement(document.getElementById('root'))
-    let id = props.id
+	const itemListContext = useContext(ItemListContext)
+	const setToDoItems = itemListContext.setToDoItems;
+	const toDoItems = itemListContext.toDoItems;
+	Modal.setAppElement(document.getElementById('root'))
+	let id = props.id
 
-    const date = new Date()
-    const newDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}T${date.getHours()}:${date.getMinutes()}`
-    
+	const date = new Date()
+	const newDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}T${date.getHours()}:${date.getMinutes()}`
+
+	const [data, setData] = useState([])
+
 	const [item, setItem] = useState({
-        "task": '',
-        "startTime": newDate,
-        "endTime": newDate
-    })
+		"task": '',
+		"startTime": newDate,
+		"endTime": newDate
+	})
 
-    const handleUpdate = (e) => {
-        axios.put(`http://localhost:8080/updateItem/${id}`, {
-            "task": item.task,
-            "starttime": item.startTime,
-            "endtime": item.endTime,
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        setModalIsOpen(false);
-        window.location.reload();
-    }
+	const handleUpdate = (e) => {
+		axios.put(`http://localhost:8080/updateItem/${id}`, {
+			"task": item.task,
+			"starttime": item.startTime,
+			"endtime": item.endTime,
+		})
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		setModalIsOpen(false);
+		window.location.reload();
+	}
 
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
 
+	useEffect(() => {
+		async function fetchData() {
+			let result = await axios('http://localhost:8080/items')
+			for (let i = 0; i < result.data.length; i++) {
+			    let item = { "task": result.data[i].Task, "startTime": result.data[i].Starttime, "endTime": result.data[i].Endtime };
+		  	    setData(data => [...data, item])
+			}
+		}
+		fetchData()
+	}, []);
 
     return(
 	    <>
@@ -45,11 +57,11 @@ export default function UpdateButton(props){
 			    <button className='xButton' onClick={() => setModalIsOpen(!modalIsOpen)}>x</button>
 			    <form className='modalForm' data-testid='modalForm'>
 					    <label role='label'>Task</label>
-				<input type='text' className='inputTask' placeholder='Item Name' value={item.task} onChange={(e) => [setItem(item => ({...item, "task": e.target.value}))]}></input>
+				<input type='text' className='inputTask' defaultValue={data.task} value={data.task} onChange={(e) => [setItem(item => ({...item, "task": e.target.value}))]}></input>
 				<label role='label'>Start Time</label>
-				<input type='datetime-local' role='input' className='inputStartTime' value={item.startTime} onChange={(e) => [setItem(item => ({...item, "startTime": e.target.value}))]}></input>
+				<input type='datetime-local' role='input' className='inputStartTime' defaultValue={data.startTime} value={item.startTime} onChange={(e) => [setItem(item => ({...item, "startTime": e.target.value}))]}></input>
 				<label role='label'>End Time</label>
-				<input type='datetime-local' role='input' className='inputEndTime' value={item.endTime} onChange={(e) => [setItem(item => ({...item, "endTime": e.target.value}))]}></input>
+				<input type='datetime-local' role='input' className='inputEndTime' defaultValue={data.endTime} value={item.endTime} onChange={(e) => [setItem(item => ({...item, "endTime": e.target.value}))]}></input>
 				<br />
 				<button onClick={()=>handleUpdate(id)}>Update</button>
 			    </form>
